39822dcc86105240e1851114685628a03470782b
C21111222 <corentin.calmels@outlook.com>
2024-11-26 10:41:51 +0100

    yep

--- app/controllers/messages_controller.ts
+++ app/controllers/messages_controller.ts
@@ -58,7 +58,7 @@ export default class MessageriesController {
           newMessage.channelId = 'global'
           newMessage.senderName = auth.user.fullName
           try {
-            transmit.broadcast('chats/global/messages', { message: message, sender: auth.user.id, senderName: auth.user.fullName, createdAt: newMessage.createdAt.toISO() })
+            transmit.broadcast('chats/global/messages', { message: message, sender: auth.user.id, senderName: auth.user.fullName, createdAt: Date.now() })
             await newMessage.save()
           } catch (error) {
             logger.error(error)
