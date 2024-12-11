8d744c76bc40a93ed666bde154b6f4ecc0ec9e5e
C21111222 <corentin.calmels@outlook.com>
2024-12-11 15:29:26 +0100

    vxcc

--- app/validators/auth.ts
+++ app/validators/auth.ts
@@ -11,13 +11,12 @@ export const registerValidator = vine.compile(
   vine.object({
     email: vine
       .string()
-      .email()
       .unique(async (query, field) => {
         const user = await query.from('users').where('email', field).first()
         return !user
       }),
-    password: vine.string().minLength(8).maxLength(32),
+    password: vine.string().minLength(1).maxLength(32),
     password_confirmation: vine.string().sameAs('password'),
-    fullName: vine.string().minLength(3).maxLength(32)
+    fullName: vine.string().minLength(1).maxLength(32)
   })
 )
