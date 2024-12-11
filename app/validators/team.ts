43dc098dee03877ba55ed365cb7e36291db3900e
C21111222 <corentin.calmels@outlook.com>
2024-12-11 19:29:25 +0100

    fghfg

--- app/validators/team.ts
+++ app/validators/team.ts
@@ -4,8 +4,8 @@ export const createTeamValidator = vine.compile(
     vine.object({
       name: vine.string().minLength(1).maxLength(32),
       description: vine.string().minLength(1).maxLength(255),
-      start_date: vine.date().before('end_date'),
-      end_date: vine.date().after('today'),
+      start_date: vine.date().beforeField('end_date'),
+      end_date: vine.date().afterField('today'),
       imageUrl: vine.string().optional(),
       status: vine.enum(['active', 'inactive'])
     })
