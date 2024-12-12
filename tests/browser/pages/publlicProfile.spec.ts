ae71328bd692a3ce0e107363857a78f253eb5a0e
C21111222 <corentin.calmels@outlook.com>
2024-12-12 11:17:19 +0100

    sdds

---
+++ tests/browser/pages/publlicProfile.spec.ts
@@ -0,0 +1,23 @@
+import { test } from '@japa/runner'
+import User from '#models/user'
+
+test.group('Public profile', () => {
+  test('displays user profile', async ({ visit }) => {
+    const user = new User()
+    user.email = 'test1@test.com'
+    user.password = 'secret'
+    user.save()
+    const page = await visit('/profile')
+    await page.fill('input[name="email"]', user.email)
+    await page.fill('input[name="password"]', user.password)
+    await page.click('button[type="submit"]')
+    // on va sur la page de profil
+    const page1 = await visit('/profile')
+    await page1.assertTextContains('body', user.email)
+})
+
+test('unauthenticated user is redirected to login page', async ({ visit }) => {
+    const page = await visit('/profile')
+    await page.assertPath('/login')
+})
+})
