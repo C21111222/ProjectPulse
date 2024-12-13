35bff75f0670d39f81bb3f726eaac1f71ced6d3c
C21111222 <corentin.calmels@outlook.com>
2024-12-13 13:02:23 +0100

    ds

--- start/routes.ts
+++ start/routes.ts
@@ -52,7 +52,7 @@ router.group(() => {
 })
 
 router.get('/docs' , async ({response}) => {
-  return response.download(app.makePath('/docs/documentation.pdf'))
+  return response.download(app.makePath('/docs/ProjectPulse.pdf'))
 })
 
 /*
